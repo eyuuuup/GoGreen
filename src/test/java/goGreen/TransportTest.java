39e35bcb2ddd6ddb3a337f78c3c0d42edb428203
gbvandam <g.b.vandam@student.tudelft.nl>
2019-02-26 10:21:19 +0100

    Added TransportTest (#39)

---
+++ src/test/java/goGreen/TransportTest.java
@@ -0,0 +1,48 @@
+package goGreen;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TransportTest {
+
+    @Test
+    public void addCycleAction() {
+        assertEquals(Transport.addCycleAction(), 100);
+    }
+
+    @Test
+    public void addCarAction() {
+        assertEquals(Transport.addCarAction(), 25);
+    }
+
+    @Test
+    public void addPlaneAction() {
+        assertEquals(Transport.addPlaneAction(), 0);
+    }
+
+    @Test
+    public void addPublicTransportAction() {
+        assertEquals(Transport.addPublicTransportAction(), 75);
+    }
+
+    @Test
+    public void addCycleActionWrong() {
+        assertNotEquals(Transport.addCycleAction(), 25);
+    }
+
+    @Test
+    public void addCarActionWrong() {
+        assertNotEquals(Transport.addCarAction(), 100);
+    }
+
+    @Test
+    public void addPlaneActionWrong() {
+        assertNotEquals(Transport.addPlaneAction(), 20);
+    }
+
+    @Test
+    public void addPublicTransportActionWrong() {
+        assertNotEquals(Transport.addPublicTransportAction(), 57);
+    }
+}
