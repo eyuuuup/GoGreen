1bf292e13dde3a547f02cf0e13b915b1f19bff3c
gbvandam <g.b.vandam@student.tudelft.nl>
2019-03-04 19:21:47 +0100

    Communication is now static

--- src/main/java/gogreen/Communication.java
+++ src/main/java/gogreen/Communication.java
@@ -1,4 +1,13 @@
 package gogreen;
 
 public class Communication {
+    private static String name;
+
+    public static void addAction(String action, int points){
+
+    }
+
+    public static void login(String username){
+
+    }
 }
