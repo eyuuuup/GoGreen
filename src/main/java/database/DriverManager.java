14b2c93fb9ac795db987b7d7fc9fca46320754d9
Eyüp Vroegop <e.vroegop@student.tudelft.nl>
2019-03-12 00:09:20 +0100

    Updated the Database and DriverManager methods
    Added all Database methods and solver all checkstyle warnings in both methods

--- src/main/java/database/DriverManager.java
+++ src/main/java/database/DriverManager.java
@@ -3,14 +3,26 @@ package database;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+
+/**
+ * DriverManager is a class that sets up the connection to the database.
+ */
 public class DriverManager   {
 
+    /**
+     * This method sets up the actual connect with the information provided.
+     * @return A Connection object.
+     */
     public static Connection getConnection() throws SQLException {
 
-        Connection conn = java.sql.DriverManager.getConnection("jdbc:postgresql://ec2-54-75-232-114.eu-west-1.compute.amazonaws.com:5432/d763drd7k09df2",
-                "sdglqfxhmgoxdy", "86bf9899b7ba509490d36a1735109060270adefe7db96bdfc2b59c959b069e3d");
+        Connection con =
+                java.sql.DriverManager.getConnection(
+                        "jdbc:postgresql://ec2-54-75-232-114.eu-west-1.compute.amazonaws.com:"
+                                + "5432/d763drd7k09df2",
+                        "sdglqfxhmgoxdy",
+                        "86bf9899b7ba509490d36a1735109060270adefe7db96bdfc2b59c959b069e3d");
        
         System.out.println("Connected to database");
-        return conn;
+        return con;
     }
 }
