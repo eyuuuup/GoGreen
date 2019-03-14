85bf171214041f32e777b72054a36117ae08c742
gbvandam <g.b.vandam@student.tudelft.nl>
2019-03-14 15:24:32 +0100

    Setup for the User

---
+++ src/test/java/client/UserTest.java
@@ -0,0 +1,24 @@
+package client;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void changeName() {
+    }
+
+    @Test
+    public void getPassword() {
+    }
+
+    @Test
+    public void changePassword() {
+    }
+}
