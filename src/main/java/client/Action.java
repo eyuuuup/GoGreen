fe665899defe7141a5970d2749734fcc163feffd
mmatusovic <m.matusovic@student.tudelft.nl>
2019-03-14 19:19:35 +0100

    Fix action class

--- src/main/java/client/Action.java
+++ src/main/java/client/Action.java
@@ -1,37 +1,39 @@
 package client;
 
-class Action {
+public class Action {
     private String user;
     private String action;
     private int    value;
 
-    Action(String user, String action, int value) {
+    public Action(String user, String action, int value) {
         this.user = user;
         this.action = action;
         this.value = value;
     }
 
-    String getUser() {
+    public Action(){}
+
+    public String getUser() {
         return user;
     }
 
-    void setUser(String user) {
+    public void setUser(String user) {
         this.user = user;
     }
 
-    String getAction() {
+    public String getAction() {
         return action;
     }
 
-    void setAction(String action) {
+    public void setAction(String action) {
         this.action = action;
     }
 
-    int getValue() {
+    public int getValue() {
         return value;
     }
 
-    void setValue(int value) {
+    public void setValue(int value) {
         this.value = value;
     }
 }
