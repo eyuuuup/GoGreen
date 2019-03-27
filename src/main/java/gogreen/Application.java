672cf11a9829f08b87a1a1e9dbe020759e06b650
Marit Radder <meradder@tudelft.nl>
2019-03-27 19:38:54 +0100

    Made the about page (#151) and gave brighter planet credits (#137)

--- src/main/java/gogreen/Application.java
+++ src/main/java/gogreen/Application.java
@@ -304,9 +304,14 @@ public class Application extends javafx.application.Application {
         Tab settingsTab = new Tab();
         settingsTab.setText("Settings");
         settingsTab.setContent(settingsScreen());
+
+        // make the about tab
+        Tab aboutTab = new Tab();
+        aboutTab.setText("About");
+        aboutTab.setContent(aboutScreen());
         
         // adds the tabs to the navigation bar
-        homeNavigation.getTabs().addAll(homeTab, settingsTab);
+        homeNavigation.getTabs().addAll(homeTab, settingsTab, aboutTab);
         
         // makes the page and adds the nodes
         Pane homePage = new Pane();
@@ -334,9 +339,9 @@ public class Application extends javafx.application.Application {
 
 
         // make the your world view
-        Image image = new Image(planetLink);
+        Image world = new Image(planetLink);
         ImageView yourWorldView = new ImageView();
-        yourWorldView.setImage(image);
+        yourWorldView.setImage(world);
 
         // makes the level bar
         ProgressBar levelBar = new ProgressBar(ApplicationMethods.getLevelProgress(points));
@@ -428,6 +433,46 @@ public class Application extends javafx.application.Application {
         
         // return the page
         return settingsPage;
+    }
+
+    private static ScrollPane aboutScreen(){
+
+        Label aboutText = new Label("Welcome to our App *insert app name* \n" +
+                "One of my favorite quotes is that if you move one grain of sand in the Sahara, you changed the whole Sahara. " +
+                "And that is what we wanted to do. " +
+                "We wanted to change the world, but we realised we couldn't do it alone. " +
+                "So we decided to encourage other people to change the world with us. " +
+                "This is one of the reasons we made this app, the other reason is that we all want a good grade on our school project. \n\n" +
+                "About the content of the app: \n" +
+                "With the app you can track your CO\u2082 reduction, and score points with it." +
+                "You can also have a little competition with friends and send them challenges. \n\n" +
+                "About the team: \n" +
+                "* Eyüp, one of our database guys \n" +
+                "* Elias, our other database guy \n" +
+                "* Shruti, our server girl \n" +
+                "* Marko, our handy man with extra focus on the server \n" +
+                "* Erwin, our client and API guy \n" +
+                "* Marit, our GUI girl \n\n" +
+                "But this app wouldn't be possible without the brighter climate API");
+        aboutText.setId("aboutText");
+
+        // make the your world view
+        Image apiButton = new Image("file:src/aboutPicture/apiButton.png");
+        ImageView apiButtonView = new ImageView();
+        apiButtonView.setImage(apiButton);
+
+        aboutText.setWrapText(true);
+
+        VBox aboutContainer = new VBox();
+        aboutContainer.setMaxWidth(475);
+        aboutContainer.getChildren().addAll(aboutText, apiButtonView);
+        aboutContainer.setAlignment(Pos.CENTER);
+
+        ScrollPane aboutPage = new ScrollPane();
+        aboutPage.setMaxSize(500,500);
+        aboutPage.setContent(aboutContainer);
+
+        return aboutPage;
     }
     
     /**
