14e9253c2fc955926bb133f5cb447817833c5f54
shrutiarora <s.arora-2@student.tudelft.nl>
2019-04-03 16:33:14 +0200

    class for challenges added

---
+++ src/main/java/client/ChallengesList.java
@@ -0,0 +1,28 @@
+package client;
+
+import java.util.ArrayList;
+
+
+/**
+ * This class is for the list of challenges
+ */
+public class ChallengesList {
+    ArrayList<Challenge> list;
+
+    public ChallengesList() {
+        list = new ArrayList<Challenge>();
+    }
+
+    public ChallengesList(ArrayList<Challenge> list) {
+        this.list = list;
+    }
+
+
+    public ArrayList<Challenge> getList() {
+        return list;
+    }
+
+    public void setList(ArrayList<Challenge> list) {
+        this.list = list;
+    }
+}
