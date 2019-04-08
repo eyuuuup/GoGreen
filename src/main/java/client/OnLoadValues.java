5b46f04b82e1f34192f09f184973480212e972d8
shrutiarora <s.arora-2@student.tudelft.nl>
2019-04-08 17:29:27 +0200

    added new fields to onLoad class

--- src/main/java/client/OnLoadValues.java
+++ src/main/java/client/OnLoadValues.java
@@ -9,19 +9,30 @@ public class OnLoadValues {
     private FriendsList followers;
     private FriendsList following;
     private ChallengesList challenges;
+    private boolean solarCar;
+    private boolean electricCar;
+    private boolean envGroup;
 
-    public OnLoadValues(User user, FriendsList followers, FriendsList following, ChallengesList challenges) {
-        this.user = user;
-        this.followers = followers;
-        this.following = following;
-        this.challenges = challenges;
-    }
+
 
     public OnLoadValues() {
         user = new User();
         followers = new FriendsList();
         following = new FriendsList();
         challenges = new ChallengesList();
+        solarCar=false;
+        electricCar=false;
+        envGroup=false;
+    }
+
+    public OnLoadValues(User user, FriendsList followers, FriendsList following, ChallengesList challenges, boolean solarCar, boolean electricCar, boolean envGroup) {
+        this.user = user;
+        this.followers = followers;
+        this.following = following;
+        this.challenges = challenges;
+        this.solarCar = solarCar;
+        this.electricCar = electricCar;
+        this.envGroup = envGroup;
     }
 
     public User getUser() {
--- src/main/java/server/OnLoadValues.java
+++ src/main/java/server/OnLoadValues.java
@@ -4,28 +4,35 @@ package server;
  * This class is for the on load values.
  */
 public class OnLoadValues {
-    /**
-     * solar panels
-     * electric car
-     * envoirment group
-     */
+
     private User user;
     private FriendsList followers;
     private FriendsList following;
     private ChallengesList challenges;
+    private boolean solarCar;
+    private boolean electricCar;
+    private boolean envGroup;
 
-    public OnLoadValues(User user, FriendsList followers, FriendsList following, ChallengesList challenges) {
-        this.user = user;
-        this.followers = followers;
-        this.following = following;
-        this.challenges = challenges;
-    }
+
 
     public OnLoadValues() {
         user = new User();
         followers = new FriendsList();
         following = new FriendsList();
         challenges = new ChallengesList();
+        solarCar=false;
+        electricCar=false;
+        envGroup=false;
+    }
+
+    public OnLoadValues(User user, FriendsList followers, FriendsList following, ChallengesList challenges, boolean solarCar, boolean electricCar, boolean envGroup) {
+        this.user = user;
+        this.followers = followers;
+        this.following = following;
+        this.challenges = challenges;
+        this.solarCar = solarCar;
+        this.electricCar = electricCar;
+        this.envGroup = envGroup;
     }
 
     public User getUser() {
