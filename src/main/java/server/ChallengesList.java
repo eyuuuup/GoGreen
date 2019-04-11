c11f5cfd2d299dc9d342ffe545ab4679e03276d6
Marko Matusovic <m.matusovic@student.tudelft.nl>
2019-04-11 02:13:46 +0200

    Configure ChallengeList holder classes (#175)

--- src/main/java/server/ChallengesList.java
+++ src/main/java/server/ChallengesList.java
@@ -8,24 +8,13 @@ import java.util.ArrayList;
 public class ChallengesList {
 
     ArrayList<Challenge> list;
-    ArrayList<Challenge> receivedList;
 
     public ChallengesList() {
         list = new ArrayList<Challenge>();
-        receivedList= new ArrayList<Challenge>();
     }
 
-    public ChallengesList(ArrayList<Challenge> list, ArrayList<Challenge> receivedList) {
+    public ChallengesList(ArrayList<Challenge> list) {
         this.list = list;
-        this.receivedList = receivedList;
-    }
-
-    public void setReceivedList(ArrayList<Challenge> receivedList) {
-        this.receivedList = receivedList;
-    }
-
-    public ArrayList<Challenge> getReceivedList() {
-        return receivedList;
     }
 
     public ArrayList<Challenge> getList() {
